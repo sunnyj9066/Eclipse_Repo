45508b30813678525fad247860a0bd1cef332f98
sunny <sunnyj9066@gmail.com>
2017-09-10 20:26:43 +0530

    initial commit

---
+++ src/test/Git_demo.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Git_demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a =10;
+		int b =20;
+		int c =a+b;
+		System.out.println(c);
+		
+	}
+
+}
